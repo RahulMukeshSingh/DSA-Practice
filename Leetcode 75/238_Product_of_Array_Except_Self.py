f373ebc929be268c0d46f498909e7401b378a555
Rahul Mukesh Singh <rahulmukeshsingh12@gmail.com>
2026-02-26 01:29:37 +0530

    Non Space Optimised Version

--- Leetcode 75/238_Product_of_Array_Except_Self.py
+++ Leetcode 75/238_Product_of_Array_Except_Self.py
@@ -11,10 +11,10 @@ class Solution(object):
             left_product[i] = left_product[i-1] * nums[i-1]
         for i in range(num_len - 2, -1, -1):
             right_product[i] = right_product[i+1] * nums[i+1]
-        answer=[]
+        answer=[1] * num_len
         for i in range(num_len): 
-            nums[i]=left_product[i]*right_product[i]
-        return nums
+            answer[i]=left_product[i]*right_product[i]
+        return answer
     
 s = Solution()
 nums = [-1,1,0,-3,3]
