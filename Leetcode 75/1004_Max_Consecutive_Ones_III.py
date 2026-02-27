3e7eb7940a7692e46ca23e8813b090e696b21aba
Rahul Mukesh Singh <rahulmukeshsingh12@gmail.com>
2026-02-28 00:20:01 +0530

    Optimised and  readble

--- Leetcode 75/1004_Max_Consecutive_Ones_III.py
+++ Leetcode 75/1004_Max_Consecutive_Ones_III.py
@@ -5,22 +5,39 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # Version 1
+        # if not nums: return None
+        # num_of_element = len(nums)
+        # max_num_consecutive_1 = 0
+        # left_index = 0
+        # right_index = 0
+        # no_of_0_slide = 1 if nums[0] == 0 else 0
+        # cur_num_consecutive_1 = 0
+        # while right_index < num_of_element:  
+        #     if no_of_0_slide <= k:
+        #         cur_num_consecutive_1 = right_index - left_index + 1
+        #         right_index += 1
+        #         if right_index < num_of_element and  nums[right_index] == 0: no_of_0_slide += 1
+        #     elif no_of_0_slide > k:
+        #         if nums[left_index] == 0: no_of_0_slide -= 1
+        #         left_index += 1   
+        #     if cur_num_consecutive_1 > max_num_consecutive_1: max_num_consecutive_1 = cur_num_consecutive_1 
+        # return max_num_consecutive_1
+        
+        # Version 2
         if not nums: return None
         num_of_element = len(nums)
         max_num_consecutive_1 = 0
         left_index = 0
         right_index = 0
-        no_of_0_slide = 1 if nums[0] == 0 else 0
         cur_num_consecutive_1 = 0
-        while right_index < num_of_element:  
-            if no_of_0_slide <= k:
-                cur_num_consecutive_1 = right_index - left_index + 1
-                right_index += 1
-                if right_index < num_of_element and  nums[right_index] == 0: no_of_0_slide += 1
-            elif no_of_0_slide > k:
-                if nums[left_index] == 0: no_of_0_slide -= 1
-                left_index += 1   
-            if cur_num_consecutive_1 > max_num_consecutive_1: max_num_consecutive_1 = cur_num_consecutive_1 
+        for right_index in range(num_of_element):
+            if nums[right_index] == 0: k -= 1
+            while k < 0 and left_index <= right_index:
+                if nums[left_index] == 0: k += 1
+                left_index += 1
+            cur_num_consecutive_1 = right_index - left_index + 1
+            if cur_num_consecutive_1 > max_num_consecutive_1: max_num_consecutive_1 = cur_num_consecutive_1
         return max_num_consecutive_1
     
 sol = Solution()
